77582ac9c2f6f9f74d7e58a0d21bbca0e36011ab
strizhkova <strizhkova@banki.ru>
2022-12-16 00:32:15 +0300

    Написала первую задачу, урок 3

---
+++ function.js
@@ -0,0 +1,21 @@
+function sumTwoNumber(a, b) {
+    return a + b;
+}
+
+const num1 = 2;
+const num2 = 3;
+
+const sumNumber = num1 + num2;
+
+console.log(sumNumber);
+
+function minTwoNumber(a,b) {
+    return a - b;
+}
+
+console.log(minTwoNumber(6,9));
+
+function auth(username, password) {
+    
+}
+
